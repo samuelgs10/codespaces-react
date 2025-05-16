6373388844df45e96426e8f03d2e7d875249e702
samuelgs10 <samuelgomeshotmail2006@gmail.com>
2025-05-16 17:23:50 +0000

    Aula 16/05/25- Configuração de css

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,18 @@
-import './App.css';
+import "./styles/theme.css";
+import "./styles/global.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div>
+      <h1>Meu primeiro React App</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt magni
+        doloribus alias molestias. Odit non dolorum impedit! Quos numquam minus
+        ipsa laborum voluptate animi magnam nisi in veritatis, suscipit
+        voluptatibus!
+      </p>
     </div>
   );
 }
 
-export default App;
+
