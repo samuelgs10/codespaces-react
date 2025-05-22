a8a1bb55bdfb6a64f7f2c366b2041eee1f2ed419
samuelgs10 <samuelgomeshotmail2006@gmail.com>
2025-05-22 17:20:28 +0000

    Aula do dia 22/05/25

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-
+import { MyText } from "./components/MyText";
 export default function App() {
   return (
-    <div>
-      <h1>Meu primeiro React App</h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt magni
-        doloribus alias molestias. Odit non dolorum impedit! Quos numquam minus
-        ipsa laborum voluptate animi magnam nisi in veritatis, suscipit
-        voluptatibus!
-      </p>
-    </div>
+    <>
+      <MyText/>
+      <MyText/>
+    </>
   );
 }
-
-
