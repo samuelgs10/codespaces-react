ca35f1850328eeeabe7e1e09662d59edb511c132
samuelgs10 <samuelgomeshotmail2006@gmail.com>
2025-05-23 17:22:15 +0000

    AULA DO DIA 23/05/25

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,24 @@ import "./styles/theme.css";
 import "./styles/global.css";
 import { MyText } from "./components/MyText";
 export default function App() {
+    
+   const text = [{title: "Meu titulo 1", text:"Meu texto 1"},
+                 {title: "Meu titulo 2", text:"Meu texto 2"},
+                 {title: "Meu titulo 3", text:"Meu texto 3"},
+                 {title: "Meu titulo 4", text:"Meu texto 4"},
+                 {title: "Meu titulo 5", text:"Meu texto 5"}];
+   
   return (
-    <>
-      <MyText/>
-      <MyText/>
-    </>
+    <div className="container">
+      <h1>Meu App</h1>
+      <div className="text-container">
+        {text.map((item) => (
+          <MyText title={item.title}  >
+            {item.text}
+          </MyText>
+        ))}
+      </div>
+    </div>
+
   );
 }
--- src/components/MyText.jsx
+++ src/components/MyText.jsx
@@ -1,16 +1,11 @@
 import styles from './MyText.module.css';  
 
-export function MyText(){
+export function MyText({title, text, children}) {
   return ( 
   <div className={styles.container}>
   <div className={styles.div}>
-      <h1 className={styles.title}>Meu primeiro React App</h1>  
-      <p className={styles.text}>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt magni
-        doloribus alias molestias. Odit non dolorum impedit! Quos numquam minus
-        ipsa laborum voluptate animi magnam nisi in veritatis, suscipit
-        voluptatibus!
-      </p>
+      <h1 className={styles.title}>{title}</h1>  
+      <p className={styles.text}> { children} </p>
     </div>
     </div>
   );
